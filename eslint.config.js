20961fe5f013083e9779931a64cfb648e52459b3
Dflame <gflaame1@gmail.com>
2026-08-08 02:42:08 +0000

    CI: ignore Next-generated type declarations

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,23 @@
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["**/node_modules/**", "**/.next/**", "**/dist/**", "**/out/**", "contracts/**"] },
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/.next/**",
+      "**/next-env.d.ts",
+      "**/dist/**",
+      "**/out/**",
+      "contracts/**",
+    ],
+  },
   ...tseslint.configs.recommended,
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
       "@typescript-eslint/consistent-type-imports": "error",
     },
   },
